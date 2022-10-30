b083a36be764911262d3ef4241cdfcd96a477e38
eric31707 <eric31707@DESKTOP-95TUUP6>
2022-10-30 22:39:44 +0800

    加入專案檔案。

---
+++ Exec2_Message/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Exec2_Message
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.resultbtn = new System.Windows.Forms.Button();
+			this.resultlbl = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// resultbtn
+			// 
+			this.resultbtn.Location = new System.Drawing.Point(84, 75);
+			this.resultbtn.Name = "resultbtn";
+			this.resultbtn.Size = new System.Drawing.Size(99, 34);
+			this.resultbtn.TabIndex = 0;
+			this.resultbtn.Text = "顯示";
+			this.resultbtn.UseVisualStyleBackColor = true;
+			this.resultbtn.Click += new System.EventHandler(this.resultbtn_Click);
+			// 
+			// resultlbl
+			// 
+			this.resultlbl.Location = new System.Drawing.Point(236, 75);
+			this.resultlbl.Name = "resultlbl";
+			this.resultlbl.Size = new System.Drawing.Size(180, 65);
+			this.resultlbl.TabIndex = 1;
+			this.resultlbl.Text = "resultlbl";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(471, 251);
+			this.Controls.Add(this.resultlbl);
+			this.Controls.Add(this.resultbtn);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button resultbtn;
+		private System.Windows.Forms.Label resultlbl;
+	}
+}
+
---
+++ Exec2_Message/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Message
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			resultlbl.Text = String.Empty;
+		}
+
+		private void resultbtn_Click(object sender, EventArgs e)
+		{
+			string result = Greeting();
+			MessageBox.Show(result);
+			resultlbl.Text = result;
+
+		}
+
+		private string Greeting()
+		{
+			string result = String.Empty;
+			string now = "現在時間是" + DateTime.Now.ToString($"yyyy/MM/dd\r\nHH:mm:ss");
+			int hours = DateTime.Now.Hour;
+			result = now;
+			result += (hours >= 0 && hours <= 10) ? "早安" : (hours >= 11 && hours <= 16)
+				                                  ? "午安" : "晚安";
+			//if (hours >= 0 && hours <= 10)
+			//{
+			//	result += "早安";
+			//}
+			//else if (hours >= 11 && hours <= 16)
+			//{
+			//	result += "午安";
+			//}
+			//else
+			//{
+			//	result += "晚安";
+			//}
+			return result;
+		}
+	}
+}
